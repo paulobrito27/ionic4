59e58650e84691147229fdfd63ec984abffa69ee
paulobrito27 <paulojmbrito27@gmail.com>
2019-06-08 15:23:14 -0300

    Mensagem de validaçao com <ion-note>

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -23,8 +23,16 @@ export class LoginPage implements OnInit {
   private createForm(): void {
     this.autenticaForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
+  }
+
+  get email(): FormControl {
+    return this.autenticaForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.autenticaForm.get('password') as FormControl;
   }
 
   onSubmit(): void {
